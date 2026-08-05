982145f40cd684222d181584afd3323600c18a60
root <root@colorado.embratelsecurity.local>
2026-08-05 17:36:47 +0000

    test: preserve CSRF cookies in login flow

--- controllers/route_test.go
+++ controllers/route_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
 	"strings"
 	"testing"
@@ -13,7 +14,18 @@ import (
 )
 
 func attemptLogin(t *testing.T, ctx *testContext, client *http.Client, username, password, optionalPath string) *http.Response {
-	resp, err := http.Get(fmt.Sprintf("%s/login", ctx.adminServer.URL))
+	if client == nil {
+		client = &http.Client{}
+	}
+	if client.Jar == nil {
+		jar, err := cookiejar.New(nil)
+		if err != nil {
+			t.Fatalf("error creating cookie jar: %v", err)
+		}
+		client.Jar = jar
+	}
+
+	resp, err := client.Get(fmt.Sprintf("%s/login", ctx.adminServer.URL))
 	if err != nil {
 		t.Fatalf("error requesting the /login endpoint: %v", err)
 	}
@@ -22,6 +34,7 @@ func attemptLogin(t *testing.T, ctx *testContext, client *http.Client, username,
 	if got != expected {
 		t.Fatalf("invalid status code received. expected %d got %d", expected, got)
 	}
+	defer resp.Body.Close()
 
 	doc, err := goquery.NewDocumentFromResponse(resp)
 	if err != nil {
@@ -32,10 +45,6 @@ func attemptLogin(t *testing.T, ctx *testContext, client *http.Client, username,
 	if !ok {
 		t.Fatal("unable to find csrf_token value in login response")
 	}
-	if client == nil {
-		client = &http.Client{}
-	}
-
 	req, err := http.NewRequest("POST", fmt.Sprintf("%s/login%s", ctx.adminServer.URL, optionalPath), strings.NewReader(url.Values{
 		"username":   {username},
 		"password":   {password},
@@ -45,7 +54,6 @@ func attemptLogin(t *testing.T, ctx *testContext, client *http.Client, username,
 		t.Fatalf("error creating new /login request: %v", err)
 	}
 
-	req.Header.Set("Cookie", resp.Header.Get("Set-Cookie"))
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	resp, err = client.Do(req)
